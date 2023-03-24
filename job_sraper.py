8987a0f48263c8ab167288adcbc913338de4b1a6
PrOriginal <33231046+PrOriginal@users.noreply.github.com>
2023-03-24 19:06:33 +0100

    Add files via upload

--- job_sraper.py
+++ job_sraper.py
@@ -1,14 +1,12 @@
 import requests
-import pandas as pd
-import numpy as np
 from bs4 import BeautifulSoup
 import unicodedata
-import time
-
-key_words = []
-salaries = []
-full_text = []
-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import pandas as pd
+import webbrowser
+#import os
 
 
 headers = {
@@ -18,103 +16,136 @@ job_boards = {
     "Pracuj.pl": "https://www.pracuj.pl/praca/python;kw/krakow;wp?rd=30&et=17",
     "justjoin.it": "https://justjoin.it/all/all/junior?q=Python@category"
 }
+  
+titles = []
+job_urls = []
+salaries = []
+expected_tech = []
+optional_tech = []
+full_text = []
+keywords = []
+
+def parse_pracuj(url):
+    r = requests.get(url, headers=headers)
+    soup = BeautifulSoup(r.content, "html.parser")
+    get_content_pracuj(soup)
+
+def parse_justjoin(url):
+    soup = get_html_justjoin(url)
+    soup = soup.find("div", class_= "css-ic7v2w")
+    get_content_justjoin(soup)
 
 
-def parse_pracuj(soup):
-    #job_divs = soup.find('div', id="c1mk5cbj")
-    titles = []
-    urls = []
+def get_html_justjoin(url):
+    #Headless Mode
+    options = Options()
+    options.add_argument("--headless=new")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+    # Zooms out page to get all divs
+    driver.execute_script("document.body.style.zoom='25%'")
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+    return soup
 
+
+def get_content_pracuj(soup):
     for job_title in soup.find_all("h2", class_ = "b1iadbg8"):
         if job_title.find("a") != None:
             titles.append(job_title.get_text().strip()) 
         else:
             print(f"The following job offer does't contain a standard link : {job_title.get_text()} ")
     
-    for job_url in soup.find_all("a", {'class': "o1o6obw3 bwcfwrp njg3w7p"}):
-        urls.append(job_url.get("href"))
+    for job_url in soup.find_all("a", class_= "o1o6obw3 bwcfwrp njg3w7p"):
+        job_urls.append(job_url.get("href"))
 
-
-    
-
-    requirements = []
-    optional = []
-    salaries = []
-    
-    i = 0
-    for url in urls:
+    i= 0
+    for url in job_urls:
         r = requests.get(url)
         #if r.status_code() == 200:
         soup = BeautifulSoup(r.content, "html.parser")
-        # for item in soup.find_all("li", class_= "offer-viewjJiyAa offer-vieweKR6vg"):
-        #     requirements = [item.get_text()]
-        #     print(requirements)
 
         # finds all required technologies and makes them into the list
-        requirements.append([item.get_text() for item in soup.find_all("li", class_= "offer-viewjJiyAa offer-vieweKR6vg")])
+        expected_tech.append([e.get_text() for e in soup.find_all("li", class_= "offer-viewjJiyAa offer-vieweKR6vg")])
         
         # Optional technologies have the same id as a required ones. To avoid duplication if statment is added
-        optional.append([item.get_text() for item in soup.find_all("p", class_= "offer-viewU0gxPf") if item.get_text() not in requirements[i]])
+        optional_tech.append([e.get_text() for e in soup.find_all("p", class_= "offer-viewU0gxPf") if e.get_text() not in expected_tech[i]])
+        i+=1
         
         salary = soup.find("strong", class_= "offer-viewLdvtPw")
         if salary != None:
-            salaries.append(unicodedata.normalize('NFKD',salary.get_text()))
+            salaries.append(unicodedata.normalize('NFKC',salary.get_text()))
         else:
             salaries.append("Undisclosed salary")
-       
-        #salaries.append(unicodedata.normalize('NFKD',salary.get_text()))
 
-        # for salary in soup.find_all("strong", class_= "offer-viewLdvtPw"):
+def get_content_justjoin(soup):
 
-        #     normalized_salary = unicodedata.normalize('NFKD', salary.get_text())
-        #     print(normalized_salary)
+    for job_title in soup.find_all("div", class_="jss244"):
+        titles.append(job_title.get_text())
 
-        #     salaries.append([unicodedata.normalize('NFKD',item.get_text()) for item in soup.find_all("strong", class_= "offer-viewLdvtPw")])
+    a_tags = soup.find_all("a")
+    #Removing unwanted element
+    a_tags.pop()
 
-        i+=1
+    justjoin_urls = []
 
-    # print(titles)
-    # print(urls)
-    # print(len(titles))
-    # print(len(salaries))    
-    # print(len(requirements))
-    # print(len(optional))
-    # print((len(urls)))
+    for url in a_tags:
+        partial_url = url.get("href")
+        justjoin_urls.append("https://justjoin.it" + partial_url)
 
-    columns = {"Title": titles, "URL": urls, "Salary": salaries, 
-            "Required_skills ": requirements, "Optional skills" : optional}
+    job_urls.extend(justjoin_urls)
+    
+    for url in justjoin_urls:
+        soup = get_html_justjoin(url)
+        expected = []
+        optional = []
+        # Sorting techs. "Nice to have" to optional_tech list, others to expected_tech list
+        for div in soup.find_all("div", class_="css-1xm32e0"):
+            tech = div.find("div", class_= "css-1eroaug").get_text()
+            lvl = div.find("div", class_="css-19mz16e").get_text()
+            if lvl != "nice to have":
+                optional.append(tech)
+            else:
+                expected.append(tech)
+        expected_tech.append(expected)
+        optional_tech.append(optional)
+
+        salaries.append([unicodedata.normalize('NFKD', e.get_text()) for e in soup.find_all("span", class_="css-a2pcn2")])
 
 
-    df_analyst = pd.DataFrame(columns)
-
-    def make_clickable(val):
-        return "<a href='{0}'>{0}</a>".format(val)
+    
 
 
-    # pd.reset_option("display.max_rows", None)
-    # pd.reset_option("display.max_columns", None)
-    # pd.reset_option("display.width", None)
-    # pd.reset_option("display.max_colwidth", -1)
-
-    df_analyst.style.format({"URL" :make_clickable})
-
-    print(df_analyst)
-
-        
-        
-
-def parse_justjoin(soup):
-    pass
 
 for name, url in job_boards.items():
-    page = 0
-    
-    r = requests.get(url, params = {"page" : page+1}, headers=headers)
-    soup = BeautifulSoup(r.content, "html.parser")
-
     if name == "Pracuj.pl":
-        parse_pracuj(soup)
-    else:
-        parse_justjoin(soup)
-    
+        pass
+        parse_pracuj(url)
+    elif name == "justjoin.it":
+        parse_justjoin(url)  
 
+print(len(titles))
+print(len(job_urls))
+print(len(salaries))
+print(len(expected_tech))
+print(len(optional_tech))
+
+columns = {"Title": titles, "URL": job_urls, "Salary": salaries, 
+        "Expected technologies": expected_tech, "Optional technologies" : optional_tech}
+
+df = pd.DataFrame(columns)
+
+def make_clickable(val):
+    return "<a href='{0}'>{0}</a>".format(val)
+
+
+# pd.reset_option("display.max_rows", None)
+# pd.reset_option("display.max_columns", None)
+# pd.reset_option("display.width", None)
+# pd.reset_option("display.max_colwidth", -1)
+
+df.style.format({"URL" :make_clickable})
+
+html = df.style.format({"URL" :make_clickable}).to_html()
+text_file = open("index.html", "w", encoding='utf-8')
+text_file.write(html)
+text_file.close()
