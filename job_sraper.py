b06a5f65a7d5c69219043bb6fc05bd6dc2a23873
PrOriginal <33231046+PrOriginal@users.noreply.github.com>
2023-03-25 20:38:28 +0100

    Update job_sraper.py

--- job_sraper.py
+++ job_sraper.py
@@ -12,7 +12,16 @@ job_boards = {
     "Pracuj.pl": "https://www.pracuj.pl/praca/python;kw/krakow;wp?rd=30&et=17",
     "justjoin.it": "https://justjoin.it/all/all/junior?q=Python@category"
 }
-  
+css_styles = [{"selector": "",
+        "props":[("border-collapse","collapse")]
+        },
+        {"selector": "th, td",
+        "props":[("padding", "5px"),("border", "solid 1px #777")]
+        },
+        {"selector":"th",
+        "props":[("background-color","lightblue")]
+        }]
+
 titles = []
 job_urls = []
 salaries = []
@@ -56,25 +65,14 @@ def get_content_pracuj(soup):
     i= 0
     for url in job_urls:
         r = requests.get(url, headers=headers)
-        #if r.status_code() == 200:
+        if r.status_code != 200:
+            print(f"ERROR :  {url}")
         soup = BeautifulSoup(r.content, "html.parser")
-
         # finds all required technologies and makes them into the list
-        expected =[e.get_text() for e in soup.find_all("li", class_= "offer-viewjJiyAa offer-vieweKR6vg")]
-        
-        if expected:
-            expected_tech.append(expected)
-        else:
-            expected_tech.append("Not specified")    
-
+        expected_tech.append([e.get_text() for e in soup.find_all("li", class_= "offer-viewjJiyAa offer-vieweKR6vg")])
         # Optional technologies have the same id as a required ones. To avoid duplication if statment is added
-        optional= [e.get_text() for e in soup.find_all("p", class_= "offer-viewU0gxPf") if e.get_text() not in expected_tech[i]]
+        optional_tech.append([e.get_text() for e in soup.find_all("p", class_= "offer-viewU0gxPf") if e.get_text() not in expected_tech[i]])
         i+=1
-        
-        if optional:
-            optional_tech.append(optional)
-        else:
-            optional_tech.append("Not specified") 
 
         salary = soup.find("strong", class_= "offer-viewLdvtPw")
         if salary:
@@ -83,23 +81,19 @@ def get_content_pracuj(soup):
             salaries.append("Undisclosed salary")
 
 def get_content_justjoin(soup):
-    for job_title in soup.find_all("div", class_="jss244"):
-        titles.append(job_title.get_text())
-
     #Removing unwanted element
     a_tags = soup.find_all("a")
     a_tags.pop()
-
     justjoin_urls = []
 
     for url in a_tags:
         partial_url = url.get("href")
         justjoin_urls.append("https://justjoin.it" + partial_url)
-
     job_urls.extend(justjoin_urls)
     
     for url in justjoin_urls:
         soup = get_html_justjoin(url)
+        titles.append(soup.find("div", class_= "css-1id4k1").get_text())
         expected = []
         optional = []
         # Sorting techs. "Nice to have" to optional_tech list, others to expected_tech list
@@ -110,17 +104,13 @@ def get_content_justjoin(soup):
                 optional.append(tech)
             else:
                 expected.append(tech)
-
-        if expected:        
-            expected_tech.append(expected)
-        else:
-            expected_tech.append("Not specified")
-        if optional:
-            optional_tech.append(optional)
-        else:
-            optional_tech.append("Not specified")
+       
+        expected_tech.append(expected)
+        optional_tech.append(optional)
 
         salary = soup.find("span", class_="css-a2pcn2")
+        if not salary:
+            print(url)
         salaries.append(unicodedata.normalize('NFKD', salary.get_text()))
 
 for name, url in job_boards.items():
@@ -129,11 +119,16 @@ for name, url in job_boards.items():
     elif name == "justjoin.it":
         parse_justjoin(url)  
 
+# Making Pandas DataFrame to visualize data in the HTML table
 columns = {"Title": titles, "URL": job_urls, "Salary": salaries, 
         "Expected technologies": expected_tech, "Optional technologies" : optional_tech}
 
 df = pd.DataFrame(columns)
-html = df.style.format({"URL" :make_clickable}).to_html()
+df.index = range(1, len(df)+1)
+df["Expected technologies"] =  df["Expected technologies"].apply(', '.join).replace("", "Not specified")
+df["Optional technologies"] =  df["Optional technologies"].apply(', '.join).replace("", "Not specified")
+df = df.style.format(hyperlinks = "html").set_table_styles(css_styles)
+html = df.to_html()
 text_file = open("index.html", "w", encoding='utf-8')
 text_file.write(html)
 text_file.close()
